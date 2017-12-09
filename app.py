d006e16768a5d02ab9acb5722247adf3ea2e65fd
amlongjie <15101186970@163.com>
2017-12-09 16:04:15 +0800

    添加首页

--- app.py
+++ app.py
@@ -16,6 +16,15 @@ def encoder(obj):
         raise TypeError('%r is not JSON serializable' % obj)
 
 
+@app.route('/index')
+def q_index():
+    db = database.Connection(host="127.0.0.1",
+                             database='blockindex',
+                             user='root',
+                             password='123456')
+    datas = db.query("SELECT createtime,idx FROM bindex")
+    return json.dumps(datas, default=encoder)
+
 @app.route('/')
 def q_index():
     db = database.Connection(host="127.0.0.1",
