94189295c5fb7c0ec8e1339ac3ed165e2cc8b984
amlongjie <15101186970@163.com>
2017-12-09 15:21:10 +0800

    fix sql

--- index.sql
+++ index.sql
@@ -3,6 +3,6 @@ create table bindex (
 		createtime datetime NOT NULL DEFAULT '1970-01-01 00:00:00',
 		idx bigint(14),
 		PRIMARY KEY (id)
-)
+);
 
-INSERT INTO bindex (createtime,idx) VALUES('2017-12-09 00:00:00',1000)
+INSERT INTO bindex (createtime,idx) VALUES('2017-12-09 00:00:00',1000);
